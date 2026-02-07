7f7a4d9f30202311645b4a65289e79ef71f4c17d
zeronokamitinh-sudo <zeronokami.tinh@gmail.com>
2026-02-07 19:33:25 +0700

    Spamsms.sh

--- Spamsms.sh
+++ Spamsms.sh
@@ -5,7 +5,7 @@ if [ -e "/data/data/com.termux/files/home/storage" ]; then
 fi
 termux-setup-storage
 yes | pkg update
-. <(curl https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/termux-change-repo.sh)
+. <(curl https://raw.githubusercontent.com/zeronokamitinh-sudo/Tool/refs/heads/main/termux-change-repo.sh)
 yes | pkg upgrade
 yes | pkg i python
 yes | pkg i python-pip
