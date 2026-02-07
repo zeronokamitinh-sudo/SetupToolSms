ff1bf72d5f8248f128b344aa1054c6f5f5eb7901
zeronokamitinh-sudo <zeronokami.tinh@gmail.com>
2026-02-07 18:21:12 +0700

    Spamsms.sh

--- Spamsms.sh
+++ Spamsms.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+cd
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/termux-change-repo.sh)
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i python-pip
+pip install requests rich prettytable pytz
+export CFLAGS="-Wno-error=implicit-function-declaration"
+pip install psutil
+curl -Ls "https://raw.githubusercontent.com/zeronokamitinh-sudo/ToolSms/refs/heads/main/spamsms.py" -o /sdcard/Download/spamsms.py
